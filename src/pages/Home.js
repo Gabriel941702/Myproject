e1d86eee4365b10f54b204523ab66933d5270953
Gabriel941702 <84083522+Gabriel941702@users.noreply.github.com>
2024-06-27 20:21:59 +0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ const articles = [
   {
     title: "Discover the History",
     content: "Georgia is a country with a rich history that spans centuries. Explore ancient landmarks and learn about its fascinating past.",
-    link: "/History"
+    link: "/scr/History"
   },
   {
     title: "Experience the Culture",
